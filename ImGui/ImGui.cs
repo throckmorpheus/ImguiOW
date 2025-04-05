fc445a90a6447fc72bf20e52bb9615e031051882
Finn Else-McCormick <2200864@uad.ac.uk>
2025-04-05 22:42:56 +0100

    Initial commit

---
+++ ImGui/ImGui.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+using ImGuiNET;
+using UnityEngine.Rendering;
+
+namespace OWImGui;
+
+public class Main : ModBehaviour
+{
+	public static Main Instance;
+
+	private CommandBuffer _renderBuffer;
+
+	public void Awake()
+	{
+		Instance = this;
+	}
+
+	public void Start()
+	{
+		ModHelper.Console.WriteLine($"Imgui loaded.", MessageType.Success);
+
+		//new Harmony("Throckmorpheus.Imgui").PatchAll(Assembly.GetExecutingAssembly());
+	}
+}
+
